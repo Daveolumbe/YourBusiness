bb0eb08ce9b8e3da50f1ac1e060fe149be32fabf
David Olumbe <david@twilightstunt.com>
2015-10-21 23:15:29 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,19 +4,19 @@
 function Validate(userReg) {
 
 
-	var emailerror = document.getElementById("emailtxt").value;
+	var emailerror = document.getElementById("email").value;
 
 
 
 	if (emailerror === "") {
-		document.getElementById("emailtxt").style.borderColor  = "#ec1e52";
+		document.getElementById("email").style.borderColor  = "#ec1e52";
 	}
 
 
 
 
 	else {
-		document.getElementById("emailtxt").style.borderColor  = "";
+		document.getElementById("email").style.borderColor  = "";
 	}
 
 
@@ -29,3 +29,23 @@ function Validate(userReg) {
 
 
 };
+
+
+// create angular app
+    
+    var validationApp = angular.module('validationApp', ['ngRoute']);
+
+    // create angular controller
+    validationApp.controller('mainController', function($scope) {
+
+        // function to submit the form after all validation has occurred            
+        $scope.submitForm = function() {
+
+            // check to make sure the form is completely valid
+            if ($scope.userReg.$valid) {
+               // alert('Congrats we have received you data');
+            }
+
+        };
+
+    });
