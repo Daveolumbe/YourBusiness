d0a3bd2b2f21e0d566ada5ea4c2a65741e7131e4
David Olumbe <david.olumbe@empgroup.co.uk>
2015-10-22 09:17:49 +0100

    commit angularJs Validation
    commit angularJs Validation

--- js/apps.js
+++ js/apps.js
@@ -1,10 +1,2 @@
-var app = angular.module("MyApp", []);
 
-app.controller("User", function($scope) {
-  $scope.user = {};
-  $scope.wasSubmitted = false;
 
-  $scope.submit = function() {
-    $scope.wasSubmitted = true;
-  };
-});
