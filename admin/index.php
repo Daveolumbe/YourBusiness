ac015a00e5b9705decdf1e7689f19f9a39bc02ab
David Olumbe <david.olumbe@empgroup.co.uk>
2015-10-27 15:56:06 +0000

    commit admin login

--- admin/index.php
+++ admin/index.php
@@ -6,10 +6,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>ADMIN</title>
 
-		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css">
 		<!-- Bootstrap CSS -->
+			<link rel="shortcut icon" href="../imgs/icons/favicon.ico"/>
 		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
 
 	</head>
 	<body ng-app="validationApp" ng-controller="mainController">
@@ -17,7 +18,7 @@
 		
 		
 			<div class="container">
-				<a href="index.php"><img src="imgs/logo.png" class="img-responsive logo"></a>
+				<a href="index.php"><img src="../imgs/logo.png" class="img-responsive logo"></a>
 			</div>
 
 			
@@ -47,7 +48,8 @@
 
 		    	<?php  // Connects to your Database   
 		     	
-		     	include_once('conn.php');
+		     	$dbcon = mysql_connect("localhost", "lovee124_adyb", "NSSwxuQ))*5M");
+$db = mysql_select_db("lovee124_ybDb145", $dbcon);
 
 		     	if (isset($_POST['submit'])) {
 		     		
@@ -99,9 +101,9 @@
 		<footer style="position: fixed;">
 			<div class="container text-center">
 				<div>
-					<img src="imgs/icons/fb.png" height="35px" width="35px"> 
-					<img src="imgs/icons/tw.png" height="35px" width="35px">
-					<img src="imgs/icons/ln.png" height="35px" width="35px">
+					<img src="../imgs/icons/fb.png" height="35px" width="35px"> 
+					<img src="../imgs/icons/tw.png" height="35px" width="35px">
+					<img src="../imgs/icons/ln.png" height="35px" width="35px">
 				</div>
 				© 2015 EMP Media. All Rights Reserved. | <a href="#">Our Terms and Conditions</a>
 			</div>
@@ -109,13 +111,13 @@
 
 		</footer>
 		
-        <script src="js/angular.min.js"></script>
+        <script src="../js/angular.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0rc1/angular-route.min.js"></script>
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<!-- Bootstrap JavaScript -->
 		<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
 			
-		<script src="js/custom.js"></script>
+		<script src="../js/custom.js"></script>
 	</body>
 </html>
--- admin/view.php
+++ admin/view.php
@@ -6,10 +6,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>YOUR BUSINESS</title>
 
-		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css">
 		<!-- Bootstrap CSS -->
+			<link rel="shortcut icon" href="../imgs/icons/favicon.ico"/>
 		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
 
 	</head>
 	<body>
@@ -17,7 +18,7 @@
 		
 		
 			<div class="container">
-				<a href=""><img src="imgs/logo.png" class="img-responsive logo"></a>
+				<a href=""><img src="../imgs/logo.png" class="img-responsive logo"></a>
 			</div>
 
 			
@@ -28,7 +29,8 @@
 		<div class="container">
 			<?php
 
-			 include_once('conn.php');
+			 $dbcon = mysql_connect("localhost", "lovee124_adyb", "NSSwxuQ))*5M");
+$db = mysql_select_db("lovee124_ybDb145", $dbcon);
 	
 mysql_select_db("lovee124_ybDb145", $dbcon);
 $sql = "SELECT * FROM yb_registration";
@@ -57,16 +59,16 @@ while($records = mysql_fetch_array($myquery)){
 mysql_close($dbcon);
 ?>
 
-<a href="export.php" class="btn btn-danger btn-lg pull-right"> EXPORT DATA </a>
+<a href="../export.php" class="btn btn-danger btn-lg pull-right"> EXPORT DATA </a>
 
 		</div>
 		
 		<footer>
 			<div class="container text-center">
 				<div>
-					<img src="imgs/icons/fb.png" height="35px" width="35px"> 
-					<img src="imgs/icons/tw.png" height="35px" width="35px">
-					<img src="imgs/icons/ln.png" height="35px" width="35px">
+					<img src="../imgs/icons/fb.png" height="35px" width="35px"> 
+					<img src="../imgs/icons/tw.png" height="35px" width="35px">
+					<img src="../imgs/icons/ln.png" height="35px" width="35px">
 				</div>
 				© 2015 EMP Media. All Rights Reserved. | <a href="#">Our Terms and Conditions</a>
 			</div>
@@ -74,13 +76,13 @@ mysql_close($dbcon);
 
 		</footer>
 		
-        <script src="js/angular.min.js"></script>
+        <script src="../js/angular.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0rc1/angular-route.min.js"></script>
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<!-- Bootstrap JavaScript -->
 		<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
 			
-		<script src="js/custom.js"></script>
+		<script src="../js/custom.js"></script>
 	</body>
 </html>
